b4e1e21c961325248c0ae5cc48368cbf91f888c1
Christophe Meessen <christophe@meessen.net>
2017-09-12 11:20:31 +0200

    add test vectors with different key lengths

--- cmac_test.go
+++ cmac_test.go
@@ -3,6 +3,7 @@ package cmac
 import (
 	"bytes"
 	"crypto/aes"
+	"encoding/hex"
 	"testing"
 )
 
@@ -74,6 +75,98 @@ func TestCMAC(t *testing.T) {
 			continue
 		}
 		if !Equal(cm.Sum(nil), test.mac) {
+			t.Errorf("mac mismatch for test %d", i)
+		}
+	}
+}
+
+// Other test vectors
+// See https://github.com/ircmaxell/PHP-PasswordLib/blob/master/test/Data/Vectors/cmac-aes.sp-800-38b.test-vectors
+func TestAESCMAC2(t *testing.T) {
+	tests := []struct {
+		key, plain, mac string
+	}{
+		{
+			key:   "2b7e151628aed2a6abf7158809cf4f3c",
+			plain: "",
+			mac:   "bb1d6929e95937287fa37d129b756746",
+		},
+		{
+			key:   "2b7e151628aed2a6abf7158809cf4f3c",
+			plain: "6bc1bee22e409f96e93d7e117393172a",
+			mac:   "070a16b46b4d4144f79bdd9dd04a287c",
+		},
+		{
+			key:   "2b7e151628aed2a6abf7158809cf4f3c",
+			plain: "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411",
+			mac:   "dfa66747de9ae63030ca32611497c827",
+		},
+		{
+			key:   "2b7e151628aed2a6abf7158809cf4f3c",
+			plain: "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710",
+			mac:   "51f0bebf7e3b9d92fc49741779363cfe",
+		},
+		{
+			key:   "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+			plain: "",
+			mac:   "d17ddf46adaacde531cac483de7a9367",
+		},
+		{
+			key:   "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+			plain: "6bc1bee22e409f96e93d7e117393172a",
+			mac:   "9e99a7bf31e710900662f65e617c5184",
+		},
+		{
+			key:   "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+			plain: "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411",
+			mac:   "8a1de5be2eb31aad089a82e6ee908b0e",
+		},
+		{
+			key:   "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+			plain: "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710",
+			mac:   "a1d5df0eed790f794d77589659f39a11",
+		},
+		{
+			key:   "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+			plain: "",
+			mac:   "028962f61b7bf89efc6b551f4667d983",
+		},
+		{
+			key:   "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+			plain: "6bc1bee22e409f96e93d7e117393172a",
+			mac:   "28a7023f452e8f82bd4bf28d8c37c35c",
+		},
+		{
+			key:   "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+			plain: "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411",
+			mac:   "aaf3d8f1de5640c232f5b169b9c911e6",
+		},
+		{
+			key:   "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+			plain: "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710",
+			mac:   "e1992190549f6ed5696a2c056c315410",
+		},
+	}
+
+	for i, test := range tests {
+		refKey, _ := hex.DecodeString(test.key)
+		plain, _ := hex.DecodeString(test.plain)
+		refMac, _ := hex.DecodeString(test.mac)
+		cm, err := New(aes.NewCipher, refKey)
+		if err != nil {
+			panic(err)
+		}
+		cm.Reset()
+		n, err := cm.Write(plain)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+			continue
+		}
+		if len(plain) != n {
+			t.Errorf("got len %d, expected %d for test %d", n, len(plain), i)
+			continue
+		}
+		if !Equal(cm.Sum(nil), refMac) {
 			t.Errorf("mac mismatch for test %d", i)
 		}
 	}
